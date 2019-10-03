482dd5f4db08411d1e60515221a82ba44f194a3f
tom <tom@gmail.com>
2019-10-03 13:59:47 +0800

    UDP client網路程式

---
+++ src/aaa/bbb/UdpClient.java
@@ -0,0 +1,50 @@
+package aaa.bbb;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UdpClient {
+    public static void main(String[] args) {
+        try {
+            final int PORT = 8000;
+
+            // 建立一個DataramSocket物件
+            DatagramSocket ds = new DatagramSocket();
+
+            // 取得接收端的電腦位址
+            InetAddress address = InetAddress.getByName("10.2.8.162");
+
+            // 要求使用者輸入字串
+            String str;
+
+            do {
+                BufferedReader keyin;
+                keyin = new BufferedReader(
+                        new InputStreamReader(System.in));
+
+                System.out.print("請輸入字串：");
+                str = keyin.readLine();
+
+                // 將字串包裝成DatagramPacket物件
+                DatagramPacket dp = new DatagramPacket(str.getBytes(),	// 資料的位置
+                        str.length(),	// 資料的大小，以byte或word（可傳中文）為單位
+                        address,		// 接收端的電腦位址
+                        PORT);		// 接收端的port
+
+                // 將DatagramPacket物件送出
+                ds.send(dp);
+            } while (!str.equals(""));
+
+            System.out.println("結束！");
+
+            ds.close();
+        }
+        catch (Exception e) {
+            System.out.println("UdpClient fails.");
+        }
+
+    }
+}
